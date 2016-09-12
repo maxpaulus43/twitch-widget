19ab20dd15cb9edc2b019a57ea206a13e82e8fbd
Max <maxpaulus43@gmail.com>
2016-09-12 08:38:25 -0700

    fixed the styles, added a small image, updated the js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,68 @@
+/* jshint loopfunc: true */
 (function($) {
     "use strict";
 
-    var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-
+    var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "LobosJR", "habathcx", "NairoMK", "noobs2ninjas", "comster404"];
     var api = "https://api.twitch.tv/kraken/streams/";
+    var twitch = "https://twitch.tv/";
 
+    function pushUser(userName, userData) { 
+        $("#userList")
+            .append($("<li>").attr("id", userName)
+                .append($("<a>").attr("href", twitch + userName).text(userName)));
+
+        if (userData) {
+            if (userData.stream) {
+                $(`#${ userName }`).addClass("online");
+                $(`#${ userName }`).append(
+                    $(`<span>: ${ userData.stream.game }</span>`));
+            }
+            else {
+                $(`#${ userName }`).addClass("offline");
+            }
+        } else {
+            $(`#${ userName }`).addClass("account-closed");
+        }
+
+        console.log(userName +":", userData);
+    }
 
     function retrieveUsers() {
-        $.ajax({
 
-        });
-    }   
+        for (let i = 0; i < userNames.length; i++) {
+            $.ajax({
+                url: api + userNames[i],
+                method: "GET",
+                jsonp: true,
+                statusCode: {
+                    404: function() {
+                        pushUser(userNames[i], null);
+                    }
+                }
+            })
+            .done((data) => pushUser(userNames[i], data));
+        }
+    }
 
-    $("#refreshBtn").on("click", retrieveUsers);
+
+    ////EVENTS////
+    $(".tab").click(function() {
+        $(".tab").removeClass("selected");
+        $(this).addClass("selected");
+
+        switch(this.id) {
+            case "offline":
+                $("li.offline").show();
+                $("li.online, li.account-closed").hide();
+                break;
+            case "online":
+                $("li.online").show();
+                $("li.offline, li.account-closed").hide();
+                break;
+            case "all":
+                $("li").show();
+        }
+    });
+
+    retrieveUsers();
 })(jQuery);
